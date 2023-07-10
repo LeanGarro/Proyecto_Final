ef54883bcaef7e78ba03fe9747f7167aeb4e7cee
Leandro Garro <leandro garro>
2023-07-09 23:30:22 -0300

    urls.py creado

---
+++ project/apps/home/urls.py
@@ -0,0 +1,4 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.db import path
+
+app_name = "Home"
--- project/apps/register/models.py
+++ project/apps/register/models.py
@@ -1,22 +1,23 @@
 from django.db import models
 
 name = "Register"
-# Create your models here.
-#class paises(models.Model):
-#    id = models.AutoField(primary_key=True)
-#    nombre = models.CharField(max_length=56)
+#Create your models here.
+class paises(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=56, blank=False, null=False)
 
-#class sexos(models.Model):
-#    id = models.AutoField(primary_key=True)
-#    nombre = models.CharField(max_length=50, primary_key=True)
+class sexos(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50, blank=False, null=False)
 
 
 
-#class usuario_register(models.Model):
-#    id = models.AutoField(primary_key=True)
-#    nombre = models.CharField(max_length=40)
-#    apellido = models.CharField(max_length=40)
-#    usuario = models.CharField(max_length=15)
-#    possword = models.CharField(max_length=25)
-#    pais = models.ForeignKey(paises, null=False, blank=False, on_delete=models.SET_NULL)
-#    sexo = models.ForeignKey(sexos, null=False, blank=False, on_delete=models.SET_NULL)
+class usuario_register(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=40, blank=False, null=False)
+    apellido = models.CharField(max_length=40, blank=False, null=False)
+    usuario = models.CharField(max_length=15, blank=False, null=False)
+    possword = models.CharField(max_length=25)
+    pais = models.ForeignKey(paises, null=False, blank=False, on_delete=models.CASCADE)
+    sexo = models.ForeignKey(sexos, null=False, blank=False, on_delete=models.CASCADE)
+    nacimiento = models.DateTimeField(auto_now_add=True)
