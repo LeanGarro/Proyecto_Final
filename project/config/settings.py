eca058b01e87e70b586d5d0ca6bf7ebe462185c3
Leandro Garro <leandro garro>
2023-07-09 23:06:26 -0300

    bug solucionado

--- project/config/settings.py
+++ project/config/settings.py
@@ -15,6 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import sys
+
+APPS_DIR = BASE_DIR / "apps"
+sys.path.append(str(APPS_DIR))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -37,8 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home',
-    'register',
+    'Home',
+    'Register',
 ]
 
 MIDDLEWARE = [
