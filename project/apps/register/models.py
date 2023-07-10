6b2d7806cefae417f4d029cd7f8aa385755b6a1c
Leandro Garro <leandro garro>
2023-07-09 23:53:35 -0300

    models de register actualizado,, nacimiento agrega

--- project/apps/register/models.py
+++ project/apps/register/models.py
@@ -1,23 +1,30 @@
 from django.db import models
 
-name = "Register"
+
 #Create your models here.
 class paises(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=56, blank=False, null=False)
+    def __str__(self):
+        return self.nombre
 
 class sexos(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50, blank=False, null=False)
 
+    def __str__(self):
+        return self.nombre
 
 
 class usuario_register(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=40, blank=False, null=False)
     apellido = models.CharField(max_length=40, blank=False, null=False)
+    nacimiento = models.DateField(null=True)
     usuario = models.CharField(max_length=15, blank=False, null=False)
     possword = models.CharField(max_length=25)
     pais = models.ForeignKey(paises, null=False, blank=False, on_delete=models.CASCADE)
     sexo = models.ForeignKey(sexos, null=False, blank=False, on_delete=models.CASCADE)
-    nacimiento = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
