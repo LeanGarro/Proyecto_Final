a45f4af9459ae214de20f03d609f80da515a4f95
LeanGarro <leandro1garro@gmail.com>
2023-07-26 19:03:05 -0300

    VerProductos funcionando, falta darle estilo

--- project/apps/productos/apps.py
+++ project/apps/productos/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class ProductosConfig(AppConfig):
+
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'productos'
+
+    name = 'apps.productos'
